365e489733f6ef2ef04603cdd04bed0f7f981f94
Artem Landakov <landakov.artem@gmail.com>
2024-05-11 14:15:53 +0300

    19.7

--- src/Collector.java
+++ src/Collector.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Collector {
     //Коллекторы в java
     //Метод collect() класса Stream позволяет перейти от потоков к привычным коллекциям - List<T>,Map<T,R> и другим
@@ -12,7 +18,21 @@ public class Collector {
     //joining() - склеивает элементы потока в одну строку
     //mapping() - преобразует элементы потока в Map<K,V>
     //groupingBy() - группирует элементы, возвращает Map<K,V>
-    //
+
+
+    //Преобразование в лист и множество выглядит вполне стандартно:
+    public static void main(String[] args) {
+        ArrayList<String> e = new ArrayList<>();
+        Collections.addAll(e, "A=1", "B=2", "C=3","D==4");
+        Set<String> res = e.stream()
+                .filter(s->Character.isUpperCase(s.charAt(0)))
+                .collect(Collectors.toSet());//идентично с toList()
+        //Преобразование в мэп требует разделить ключ и значение:
+        Map<String,Integer> res2= e.stream()
+                .map(w->w.split("="))//преобразовываем каждую строку в массив строк, разделяя их по регулярному выражению
+                .filter(w->w.length==2)//пропускаем массивы, содержащие два элемента - "D==4" разбит на три элемента и не пройдёт
+                .collect(Collectors.toMap(w->w[0],w->Integer.parseInt(w[1])));//передаются две функии - одна возвращает ключ String, вторая - значение, преобразованное в Integer
+    }
     //
     //
     //
